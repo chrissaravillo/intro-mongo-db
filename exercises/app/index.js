16daf2ed34291302d963d600656b31f83a9a0d01
boyets <38266036+boyets@users.noreply.github.com>
2018-10-18 09:23:45 -0500

    Added comments

--- exercises/app/index.js
+++ exercises/app/index.js
@@ -9,12 +9,14 @@ app.use(morgan("dev"));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
+// GET http://localhost:4000/todo/5bc0ebedfe545c0c5886ddda
 app.get("/todo/:id", async (req, res) => {
   const todoId = req.params.id;
   const todo = await Todo.findById(todoId).exec();
   res.status(200).json(todo);
 });
 
+// GET http://localhost:4000/todos
 app.get("/todos", async (req, res) => {
   const todos = await Todo.find({})
     .lean()
@@ -22,6 +24,13 @@ app.get("/todos", async (req, res) => {
   res.status(200).json(todos);
 });
 
+// POST http://localhost:4000/todo/
+// {
+// 	"todo": {
+// 		"message": "test",
+// 		"complete": "true"
+// 	}
+// }
 app.post("/todo", async (req, res) => {
   const todoToCreate = req.body.todo;
   const todo = await Todo.create(todoToCreate);
